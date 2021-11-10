9818a2de47442b580779b4bde2be5cf3d8c35176
Natalia Mellino <natimellino.nb@gmail.com>
2021-11-09 21:09:41 -0300

    Added file reading for parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
+import System.Environment (getArgs)
 import Lib
+import Parse
+import CTL
 
 main :: IO ()
-main = someFunc
+main = do args <- getArgs
+          contents <- readFile $ head args
+          let model = func $ lexer contents
+          print model
+          
