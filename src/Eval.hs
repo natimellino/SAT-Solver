4a3102b7dcce62c0a3566969b3e6373919af73ec
Natalia Mellino <natimellino.nb@gmail.com>
2021-11-16 16:16:59 -0300

    Started some sat algorithms

--- src/Eval.hs
+++ src/Eval.hs
@@ -7,7 +7,8 @@ import Data.List
 -- let x = func $ lexer "STATES = [];RELATIONS = [];VALUATIONS = {};CTLEXP = AX p ;" 
 -- in eval x
 
--- TODO: revisar esto tiene q tomar un ctl ???? hay algo raro
+-- TODO: dejar de usar listas y ver de usar conjuntos si es mas eficiente
+
 eval :: Model -> [State]
 eval mdl = fst $ runState (sat (ctlExpr mdl)) mdl
 
@@ -26,12 +27,46 @@ sat (And ctl ctl') = do res <- sat ctl
 sat (Or ctl ctl') = do res <- sat ctl
                        res' <- sat ctl'
                        return $ res `union` res'
-sat (Then ctl ctl') = sat (Or (Not ctl) (ctl'))                                     
+sat (Then ctl ctl') = sat (Or (Not ctl) (ctl'))
+sat (EX ctl) = do res <- sat ctl
+                  preExists res
+sat (AX ctl) = do res <- sat ctl
+                  preForAll res                                     
 -- TODO: faltan los casos pikntes :(
 sat _ = return []
 
--- Returns the states where and atomic is valid
+-- Returns the states where an atomic is valid
 getMatchedStates :: Atomic -> [Valuation] -> [State]
 getMatchedStates _ [] = []
 getMatchedStates at ((at', states):xs) = if at == at' then states
-                                         else getMatchedStates at xs 
+                                         else getMatchedStates at xs
+
+
+-- TODO: testear estas funcionessss
+preExists :: MonadState m => [State] -> m [State]
+preExists [] = return []
+preExists (xs) = do sts <- getStates
+                    rels <- getRels
+                    return $ go sts xs rels
+                where go [] _  _= []
+                      go (s:sts) xs rels = let l = filter (\s'-> (s, s') `elem` rels ) xs
+                                               l' = go sts xs rels
+                                           in if length l > 0 then s:l'
+                                              else l'  -- Eliminar elementos repetidos
+
+preForAll :: MonadState m => [State] -> m [State]
+preForAll [] = return []
+preForAll (xs) = do sts <- getStates
+                    rels <- getRels
+                    return $ go sts xs rels
+                where go [] _  _= []
+                      go (s:sts) xs rels = let l = filter (\s'-> (s, s') `elem` rels ) xs
+                                               l' = go sts xs rels
+                                           in if length l  == length xs then s:l'
+                                              else l'  -- Eliminar elementos repetidos
+
+inev = undefined
+
+existsUntil = undefined
+
+forAllUntil = undefined
