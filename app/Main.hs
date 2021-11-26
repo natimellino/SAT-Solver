7c72184777a8caae5edc8f9f99f46f7e6abab585
Natalia Mellino <natimellino.nb@gmail.com>
2021-11-26 16:48:43 -0300

    Pretty printing for ctl formula

--- app/Main.hs
+++ app/Main.hs
@@ -17,12 +17,19 @@ main = do args <- getArgs
           let model = parseModel contents
           putStrLn $ ppPrompt "Evaluating..."
           let sts = eval model
-          print model
-          putStrLn ((ppResult "SAT: ") ++ (intersperse ' ' (concat $ toList sts)))
-
+        --   print model
+          printCTL (sctlExpr model)
+          printResult sts
 
 parseModel :: String -> SModel
 parseModel contents = let model = func $ lexer contents
                           smodel = SMdl (ctlExpr model) (fromList (sts model)) (rels model) (vals model)
                       in smodel
-          
+
+printCTL :: CTL -> IO ()
+printCTL ctl = putStrLn (ppResult  "SAT FOR " ++ (ppFormula ctl) ++ (ppResult ":"))
+
+printResult :: Set State -> IO ()
+printResult sts = go (toList sts)
+                  where go [] = putStrLn uemptyset
+                        go xs = putStrLn (intersperse ' ' (concat xs))
--- src/PPrint.hs
+++ src/PPrint.hs
@@ -6,9 +6,30 @@ import Prettyprinter.Render.Terminal
   ( renderStrict, italicized, bold, color, colorDull, Color (..), AnsiStyle )
 import Data.Text.Prettyprint.Doc
 
+ {-# LANGUAGE UnicodeSyntax #-}
+
+-- Colors
+
 errorColor = annotate (color Red)
 promptColor = annotate (color Magenta)
 resultColor = annotate (color Green)
+ctlColor = annotate (color Yellow)
+
+-- Unicode symbols for CTL formula
+
+unot = "\172"
+uemptyset = "\216"
+uexists = "\398"
+uand = " \708 "
+uor = " \709 "
+uthen = " \x2192 "
+uall = "\x2200"
+usquare = "\x218A"
+urombo = "\x25CA"
+ubottom = "\x22A5"
+utop = "\x22A4"
+ucircle = "\x25EF"
+uunion = " \x222A "
 
 -- | Pretty printer de nombres (Doc)
 prompt2doc :: String -> Doc AnsiStyle
@@ -16,6 +37,9 @@ prompt2doc n = promptColor (pretty n)
 
 result2doc :: String -> Doc AnsiStyle
 result2doc n = resultColor (pretty n)
+
+ctl2doc :: String -> Doc AnsiStyle
+ctl2doc n = ctlColor (pretty n)
 
 render :: Doc AnsiStyle -> String
 render = unpack . renderStrict . layoutSmart defaultLayoutOptions
@@ -25,6 +49,27 @@ ppPrompt x  = render $ prompt2doc x
 
 ppResult :: String -> String
 ppResult x = render $ result2doc x
+
+ppFormula :: CTL -> String
+ppFormula x = render $ ctl2doc (ppCTL x)
+
+ppCTL :: CTL -> String
+ppCTL (Atomic v) = v
+ppCTL Bottom = ubottom
+ppCTL Top = utop
+ppCTL (Not p) = unot ++ (ppCTL p)
+ppCTL (And p q) = (ppCTL p) ++ uand ++ (ppCTL q)
+ppCTL (Or p q) = (ppCTL p) ++ uor ++ (ppCTL q)
+ppCTL (Then p q) = (ppCTL p) ++ uthen ++ (ppCTL q)
+ppCTL (AX p) =  uall ++ ucircle ++ (ppCTL p)
+ppCTL (EX p) =  uexists ++ ucircle ++ (ppCTL p)
+ppCTL (AU p q) =  uall ++ "[" ++ (ppCTL p) ++ uunion ++ (ppCTL q) ++ "]"
+ppCTL (EU p q) =  uexists ++ "[" ++ (ppCTL p) ++ uunion ++ (ppCTL q) ++ "]"
+ppCTL (AF p) = uall ++ urombo ++ (ppCTL p)
+ppCTL (EF p) = uexists ++ urombo ++ (ppCTL p)
+ppCTL (AG p) = uall ++ usquare ++ (ppCTL p)
+ppCTL (EG p) = uexists ++ usquare ++ (ppCTL p)
+
 
 ppModel :: SModel -> String
 ppModel = undefined
